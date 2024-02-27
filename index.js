10be34c57e4f293f7730ab45f3f71e142922d0c1
rafagarfield <102320113+rafagarfield@users.noreply.github.com>
2024-02-27 09:34:33 -0500

    correccion js

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-class Course {
-    constructor(id, name, question, image) {
+class Course{
+    constructor(id,name,question,image){
         this.id = id;
         this.name = name;
         this.question = question;
         this.image = image;
+
     }
 }
 
 class Repository {
-    constructor() {
-        this.courses = [];
-        this.id = 0;
+    constructor(){
+        this.courses= [];
+        this.id= 0;
+
     }
 
     getAllCourses() {
-        return this.courses;
+        return this.courses
     }
 
-    createCourse( name, question, image) {
-        this.id ++;
-        const course = new Course(this.id, name, question, image);
+    createCourses(name,question,image){
+        this.id++;
+        const course= new Course(this.id, name ,question,image);
         this.courses.push(course);
+
+    }
+    deleteCourses(id){
+        this.courses=this.courses.filter(coursess => coursess.id !== id);
     }
 }
 
-const repo = new Repository(); // Creamos una instancia de Repository
-repo.createCourse( "mate", "1+1=3", "www.google.com");
-repo.createCourse( "mate", "1+1=3", "www.google.com");
-repo.createCourse( "mate", "1+1=3", "www.google.com");
-repo.createCourse( "mate", "1+1=3", "www.google.com");
-repo.createCourse( "mate", "1+1=3", "www.google.com");
-// Llamamos al método createCourse de la instancia de Repository
+/* const repo1= new Repository();
+repo1.createCourses("mate","1+1=2","www.exambhhhple.com");
+repo1.createCourses("mate","1+1=2","www.exambhhhple.com");
+repo1.createCourses("mate","1+1=2","www.exambhhhple.com");
+repo1.createCourses("mate","1+1=2","www.exambhhhple.com");
+repo1.createCourses("mate","1+1=2","www.exambhhhple.com");
+repo1.createCourses("mate","1+1=2","www.exambhhhple.com");
+repo1.createCourses("mate","1+1=2","www.exambhhhple.com");
 
-console.log(repo.getAllCourses()); // Para verificar si se creó el curso correctamente
 
+console.log(repo1.deleteCourses(2));
+console.log(repo1.getAllCourses()); */
+
+function cardGenerator(courses) {
+    const {id, name, description, image} = courses;
+
+    const card=document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+        <h3 class="card-title">${name}</h3>
+        <p class="card-text">${description}</p>
+        <img src=${image} class="card-img-top" alt="...">
+    `;
+    return card
+}
+ 
